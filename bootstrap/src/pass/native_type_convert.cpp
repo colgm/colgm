e7db3b900fc20a4875f1f70dd30b53e0eaadc6ae
ValKmjolnir <lhk101lhk101@qq.com>
2024-07-03 00:35:48 +0800

    :zap: optimize

--- bootstrap/src/pass/native_type_convert.cpp
+++ bootstrap/src/pass/native_type_convert.cpp
@@ -2,43 +2,40 @@
 
 namespace colgm {
 
-bool native_type_convert::run() {
-    static const std::unordered_map<std::string, std::string> type_mapper = {
-        {"u8", "i8"}, {"u16", "i16"},
-        {"u32", "i32"}, {"u64", "i64"},
-        {"i8", "i8"}, {"i16", "i16"},
-        {"i32", "i32"}, {"i64", "i64"},
-        {"f32", "float"}, {"f64", "double"}
-    };
-
-    for(const auto& src : ctx->used_basic_convert_method) {
-        for(const auto& dst : src.second) {
-            auto convert_function = new hir_func(
-                "@native." + src.first + "." + dst
-            );
-            convert_function->add_param("num", type_mapper.at(src.first));
-            convert_function->set_return_type(type_mapper.at(dst));
-            convert_function->set_code_block(new sir_block);
-            if (type_mapper.at(src.first)==type_mapper.at(dst)) {
-                convert_function->get_code_block()->add_stmt(new sir_ret(
-                    type_mapper.at(dst),
-                    "num"
-                ));
-            } else {
-                convert_function->get_code_block()->add_stmt(new sir_type_convert(
-                    "num",
-                    "0",
-                    type_mapper.at(src.first),
-                    type_mapper.at(dst),
-                    false
-                ));
-                convert_function->get_code_block()->add_stmt(new sir_ret(
-                    type_mapper.at(dst),
-                    "0"
-                ));
-            }
-            ctx->func_impls.push_back(convert_function);
+void native_type_convert::gen(const std::string& source,
+                              const std::unordered_set<std::string>& targets) {
+    for(const auto& target : targets) {
+        auto convert_function = new hir_func(
+            "@native." + source + "." + target
+        );
+        convert_function->add_param("num", type_mapper.at(source));
+        convert_function->set_return_type(type_mapper.at(target));
+        convert_function->set_code_block(new sir_block);
+        if (type_mapper.at(source)==type_mapper.at(target)) {
+            convert_function->get_code_block()->add_stmt(new sir_ret(
+                type_mapper.at(target),
+                "num"
+            ));
+        } else {
+            convert_function->get_code_block()->add_stmt(new sir_type_convert(
+                "num",
+                "0",
+                type_mapper.at(source),
+                type_mapper.at(target),
+                false
+            ));
+            convert_function->get_code_block()->add_stmt(new sir_ret(
+                type_mapper.at(target),
+                "0"
+            ));
         }
+        ctx->func_impls.push_back(convert_function);
+    }
+}
+
+bool native_type_convert::run() {
+    for(const auto& src : ctx->used_basic_convert_method) {
+        gen(src.first, src.second);
     }
     return true;
 }
--- bootstrap/src/pass/native_type_convert.h
+++ bootstrap/src/pass/native_type_convert.h
@@ -2,9 +2,26 @@
 
 #include "pass/pass.h"
 
+#include <cstring>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+
 namespace colgm {
 
 class native_type_convert: public pass {
+private:
+    static inline const std::unordered_map<std::string, std::string> type_mapper = {
+        {"u8", "i8"}, {"u16", "i16"},
+        {"u32", "i32"}, {"u64", "i64"},
+        {"i8", "i8"}, {"i16", "i16"},
+        {"i32", "i32"}, {"i64", "i64"},
+        {"f32", "float"}, {"f64", "double"}
+    };
+
+private:
+    void gen(const std::string&, const std::unordered_set<std::string>&);
+
 public:
     native_type_convert(ir_context& c):
         pass(pass_kind::ps_native_type_conv, c) {}
