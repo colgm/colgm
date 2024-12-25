448ab60265272f1539851e2ce057314976079f62
ValKmjolnir <lhk101lhk101@qq.com>
2024-12-26 00:40:21 +0800

    :memo: add notes

--- bootstrap/sir/adjust_va_arg_func.cpp
+++ bootstrap/sir/adjust_va_arg_func.cpp
@@ -3,6 +3,11 @@
 namespace colgm {
 
 void adjust_va_arg_func::adjust_posix_open(sir_context* ctx) {
+    // On most platforms, open uses 3 registers to pass the arguments,
+    // but on macOS aarch64, the third argument is passed on stack.
+    // If we use `open(i8*, i32, i32);`, macOS target executable will malfunction.
+    // So we change open to open(i8*, i32, ...) on all the platforms
+    // to make sure the third argument functioning properly
     for (auto i : ctx->func_impls) {
         for (auto s : i->get_code_block()->get_stmts()) {
             if (s->get_ir_type() != sir_kind::sir_call) {
