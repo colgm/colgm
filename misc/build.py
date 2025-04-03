6c4e6facd07d3e1336b78d6a4a733bac1682e37c
ValKmjolnir <lhk101lhk101@qq.com>
2025-04-04 00:17:18 +0800

    :zap: optimize structure of token

--- misc/build.py
+++ misc/build.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-def execute(cmd: list[str]):
+def execute(cmd: list[str]) -> int:
     print("Executing: " + " ".join(cmd), flush=True)
     ret = subprocess.run(cmd).returncode
     if ret != 0:
@@ -9,7 +9,7 @@ def execute(cmd: list[str]):
         exit(ret)
     return ret
 
-def find_clang():
+def find_clang() -> str:
     suppored_clang_version = []
     for i in range(13, 20):
         suppored_clang_version.append("clang-" + str(i))
@@ -28,20 +28,20 @@ BUILD_DIRECTORY = "build"
 BOOTSTRAP_COMPILER = BUILD_DIRECTORY + "/colgm"
 SELF_HOST_COMPILER = "./colgm"
 SELF_HOST_OUT = "./a.out"
+USED_CLANG = find_clang()
 
 if not os.path.exists(BUILD_DIRECTORY):
     os.mkdir(BUILD_DIRECTORY)
 
-# Build bootstrap compiler
+# Build bootstrap compiler (written in C++)
 os.chdir(BUILD_DIRECTORY)
 execute(["cmake", "../bootstrap", "-DCMAKE_BUILD_TYPE=RelWithDebInfo"])
 execute(["make", "-j6"])
 os.chdir("..")
 
-# Build colgm self-host compiler
+# Build colgm self-host compiler (written in colgm)
 execute(["./" + BOOTSTRAP_COMPILER, "--library", "src", "src/main.colgm", "-o", "colgm.ll", "--pass-info"])
-used_clang = find_clang()
-execute([used_clang, "colgm.ll", "-o", SELF_HOST_COMPILER, "-g", "-Oz", "-rdynamic", "-lm", "--verbose"])
+execute([USED_CLANG, "colgm.ll", "-o", SELF_HOST_COMPILER, "-g", "-Oz", "-rdynamic", "-lm", "--verbose"])
 
 # Test colgm self-host compiler compiling itself
 execute([SELF_HOST_COMPILER, "--library", "src", "src/main.colgm", "--verbose", "-g", "-Oz"])
