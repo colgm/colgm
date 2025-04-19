83e54a69869bdb11910df5bb2771dae81ae2ede9
ValKmjolnir <lhk101lhk101@qq.com>
2025-04-20 02:15:04 +0800

    :memo: adjust test referenced library

--- misc/test.py
+++ misc/test.py
@@ -1,8 +1,9 @@
 import subprocess
 import os
 
-def execute(cmd: list[str], allow_failure: bool) -> int:
-    print("Executing: " + " ".join(cmd), flush=True)
+def execute(cmd: list[str], allow_failure: bool, do_print: bool = True) -> int:
+    if do_print:
+        print("Executing: " + " ".join(cmd), flush=True)
     ret = subprocess.run(cmd).returncode
     if ret != 0:
         print("Error: " + " ".join(cmd), flush=True)
@@ -12,31 +13,31 @@ def execute(cmd: list[str], allow_failure: bool) -> int:
 
 TEST_LIST = [
     # file name                      | library path   | allowed to fail
-    ("test/assign.colgm",                  "test/test_lib", False),
-    ("test/bitwise.colgm",                 "test/test_lib", False),
-    ("test/branch.colgm",                  "test/test_lib", False),
-    ("test/cmpnot.colgm",                  "test/test_lib", False),
-    ("test/complex_generics.colgm",        "test/test_lib", False),
-    ("test/continue_break.colgm",          "test/test_lib", False),
-    ("test/enum_test.colgm",               "test/test_lib", False),
+    ("test/assign.colgm",                  "src",           False),
+    ("test/bitwise.colgm",                 "src",           False),
+    ("test/branch.colgm",                  "src",           False),
+    ("test/cmpnot.colgm",                  "src",           False),
+    ("test/complex_generics.colgm",        "src",           False),
+    ("test/continue_break.colgm",          "src",           False),
+    ("test/enum_test.colgm",               "src",           False),
     ("test/for_iter.colgm",                "src",           False),
-    ("test/for_test.colgm",                "test/test_lib", False),
-    ("test/func.colgm",                    "test/test_lib", False),
-    ("test/generic_array.colgm",           "test/test_lib", False),
-    ("test/generic_embed.colgm",           "test/test_lib", False),
+    ("test/for_test.colgm",                "src",           False),
+    ("test/func.colgm",                    "src",           False),
+    ("test/generic_array.colgm",           "src",           False),
+    ("test/generic_embed.colgm",           "src",           False),
     ("test/generic.colgm",                 "test/test_lib", False),
-    ("test/hello.colgm",                   "test/test_lib", False),
-    ("test/initializer.colgm",             "test/test_lib", False),
-    ("test/local.colgm",                   "test/test_lib", False),
-    ("test/match.colgm",                   "test/test_lib", False),
-    ("test/negative.colgm",                "test/test_lib", False),
-    ("test/recursive_instantiation.colgm", "test/test_lib", True),
-    ("test/self_ref_test.colgm",           "test/test_lib", False),
-    ("test/string.colgm",                  "test/test_lib", False),
-    ("test/to_str.colgm",                  "test/test_lib", False),
-    ("test/type_convert.colgm",            "test/test_lib", False),
-    ("test/void_return.colgm",             "test/test_lib", False),
-    ("test/warn_on_left_call.colgm",       "test/test_lib", False),
+    ("test/hello.colgm",                   "src",           False),
+    ("test/initializer.colgm",             "src",           False),
+    ("test/local.colgm",                   "src",           False),
+    ("test/match.colgm",                   "src",           False),
+    ("test/negative.colgm",                "src",           False),
+    ("test/recursive_instantiation.colgm", "src",           True),
+    ("test/self_ref_test.colgm",           "src",           False),
+    ("test/string.colgm",                  "src",           False),
+    ("test/to_str.colgm",                  "src",           False),
+    ("test/type_convert.colgm",            "src",           False),
+    ("test/void_return.colgm",             "src",           False),
+    ("test/warn_on_left_call.colgm",       "src",           False),
     ("src/test/test.colgm",                "src",           False)
 ]
 
@@ -53,6 +54,6 @@ for (test, lib, allow_failure) in TEST_LIST:
     execute(["./test.out"], allow_failure)
 
 if os.path.exists("test.out"):
-    execute(["rm", "test.out"], False)
+    execute(["rm", "test.out"], False, False)
 if os.path.exists("test.out.ll"):
-    execute(["rm", "test.out.ll"], False)
+    execute(["rm", "test.out.ll"], False, False)
