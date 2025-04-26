2977fd39d5399eed0c129d6686baca9e3b9a0a3f
ValKmjolnir <lhk101lhk101@qq.com>
2025-04-26 18:09:20 +0800

    :memo: add test script

---
+++ misc/test_tcp_udp.py
@@ -0,0 +1,79 @@
+from threading import Thread
+import subprocess
+import time
+
+import os
+
+def check_required():
+    if not os.path.exists("build/colgm_self_host"):
+        print("Error: build/colgm_self_host not found", flush=True)
+        exit(1)
+    if not os.path.exists("test/socket/udp_example.colgm"):
+        print("Error: test/socket/udp_example.colgm not found", flush=True)
+        exit(1)
+    if not os.path.exists("test/socket/tcp_example.colgm"):
+        print("Error: test/socket/tcp_example.colgm not found", flush=True)
+        exit(1)
+
+def compile_tcp():
+    subprocess.run([
+        "./build/colgm_self_host",
+        "--library", "src",
+        "test/socket/tcp_example.colgm"
+    ])
+
+def compile_udp():
+    subprocess.run([
+        "./build/colgm_self_host",
+        "--library", "src",
+        "test/socket/udp_example.colgm"
+    ])
+
+ERROR_RETURN = False
+
+def run_server():
+    if subprocess.run(["./a.out", "server"]).returncode != 0:
+        global ERROR_RETURN
+        ERROR_RETURN = True
+
+def run_client():
+    if subprocess.run(["./a.out", "client"]).returncode != 0:
+        global ERROR_RETURN
+        ERROR_RETURN = True
+
+def test_tcp():
+    compile_tcp()
+    server = Thread(target=run_server)
+    client = Thread(target=run_client)
+
+    server.start()
+    time.sleep(1)
+    client.start()
+
+    server.join()
+    client.join()
+
+    if ERROR_RETURN:
+        print("Error: test_tcp failed", flush=True)
+        exit(1)
+
+def test_udp():
+    compile_udp()
+    server = Thread(target=run_server)
+    client = Thread(target=run_client)
+
+    server.start()
+    time.sleep(1)
+    client.start()
+
+    server.join()
+    client.join()
+
+    if ERROR_RETURN:
+        print("Error: test_udp failed", flush=True)
+        exit(1)
+
+if __name__ == "__main__":
+    check_required()
+    test_tcp()
+    test_udp()
