0eafce7106e1b472d0e24d6a05ca06a221d3e0b6
ValKmjolnir <lhk101lhk101@qq.com>
2025-09-28 01:19:59 +0800

    :fire: delete remove_alloca pass in boot compiler

--- bootstrap/sir/pass_manager.cpp
+++ bootstrap/sir/pass_manager.cpp
@@ -1,7 +1,6 @@
 #include "sir/pass_manager.h"
 #include "sir/adjust_va_arg_func.h"
 #include "sir/delete_useless_label.h"
-#include "sir/remove_alloca.h"
 #include "sir/detect_redef_extern.h"
 #include "sir/primitive_size_opt.h"
 #include "sir/replace_ptr_call.h"
@@ -18,7 +17,6 @@ sir_pass_manager::~sir_pass_manager() {
 void sir_pass_manager::execute(sir_context* sctx, bool verbose) {
     passes.push_back(new adjust_va_arg_func);
     passes.push_back(new delete_useless_label);
-    passes.push_back(new remove_alloca);
     passes.push_back(new detect_redef_extern);
     passes.push_back(new primitive_size_opt);
     passes.push_back(new replace_ptr_call);
