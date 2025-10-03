cc4ca31546e68a7e9d9b3e2091e3f7a0b1eb12c4
ValKmjolnir <lhk101lhk101@qq.com>
2025-10-04 02:23:00 +0800

    :art: self-host on windows platform

--- misc/util/exec.py
+++ misc/util/exec.py
@@ -1,5 +1,9 @@
 import subprocess
 import os
+import sys
+
+def is_windows() -> bool:
+    return sys.platform == "win32"
 
 def execute(cmd: list[str]) -> int:
     print("Executing: " + " ".join(cmd), flush=True)
@@ -9,34 +13,49 @@ def execute(cmd: list[str]) -> int:
         exit(ret)
     return ret
 
+def get_path() -> list[str]:
+    return os.environ["PATH"].split(";" if is_windows() else ":")
+
 def find_clang() -> str:
+    suffix = ".exe" if is_windows() else ""
     suppored_clang_version = []
     for i in range(13, 20):
-        suppored_clang_version.append("clang-" + str(i))
-        suppored_clang_version.append("clang++-" + str(i))
-    suppored_clang_version.append("clang")
-    suppored_clang_version.append("clang++")
+        suppored_clang_version.append("clang-" + str(i) + suffix)
+        suppored_clang_version.append("clang++-" + str(i) + suffix)
+    suppored_clang_version.append("clang" + suffix)
+    suppored_clang_version.append("clang++" + suffix)
 
-    for path in os.environ["PATH"].split(":"):
+    for path in get_path():
         for clang in suppored_clang_version:
-            if os.path.exists(path + "/" + clang):
-                return path + "/" + clang
+            if os.path.exists(os.path.join(path, clang)):
+                return os.path.join(path, clang)
     print("Error: clang not found", flush=True)
     exit(1)
 
 def find_cmake():
-    for path in os.environ["PATH"].split(":"):
-        if os.path.exists(path + "/cmake"):
-            print("CMake: found", path + "/cmake", flush=True)
-            return
+    suffix = ".exe" if is_windows() else ""
+    for path in get_path():
+        possible_path = os.path.join(path, "cmake" + suffix)
+        if os.path.exists(possible_path):
+            print("CMake: found", possible_path, flush=True)
+            return possible_path
     print("Error: cmake not found", flush=True)
     exit(1)
 
 BUILD_DIRECTORY = "build"
-BOOTSTRAP_COMPILER = "build/colgm"           # compiler written in C++
-LIFTED_COMPILER = "build/colgm_lifted"       # compiler written in colgm, but lifted by bootstrap compiler
-SELF_HOST_COMPILER = "build/colgm_self_host" # compiler written in colgm, compiled by lifted compiler
-                                             # at this time we have the self host compiler
+if is_windows():
+    BUILD_DIRECTORY = "cmake-windows-build"
+
+BOOTSTRAP_COMPILER = os.path.join(BUILD_DIRECTORY, "colgm")           # compiler written in C++
+if is_windows():
+    BOOTSTRAP_COMPILER  = os.path.join(BUILD_DIRECTORY, "Release\colgm.exe")
+
+LIFTED_COMPILER    = os.path.join(BUILD_DIRECTORY, "colgm_lifted")    # compiler written in colgm, but lifted by bootstrap compiler
+SELF_HOST_COMPILER = os.path.join(BUILD_DIRECTORY, "colgm_self_host") # compiler written in colgm, compiled by lifted compiler
+                                                                      # at this time we have the self host compiler
+if is_windows():
+    LIFTED_COMPILER    += ".exe"
+    SELF_HOST_COMPILER += ".exe"
 USED_CLANG = find_clang()
 
 def get_used_cpu_count():
@@ -46,36 +65,55 @@ def get_used_cpu_count():
     return 1
 
 def build_bootstrap_compiler():
-    find_cmake()
+    cmake = find_cmake()
 
     if not os.path.exists(BUILD_DIRECTORY):
         os.mkdir(BUILD_DIRECTORY)
     os.chdir(BUILD_DIRECTORY)
     execute([
-        "cmake",
+        cmake,
         "../bootstrap",
         "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
     ])
-    execute(["make", "-j{}".format(get_used_cpu_count())])
+    if is_windows():
+        execute([
+            "MSBuild.exe",
+            "colgm.sln",
+            "/p:Configuration=Release",
+            "/p:Platform=x64",
+            "-maxcpucount:{}".format(get_used_cpu_count())
+        ])
+    else:
+        execute(["make", "-j{}".format(get_used_cpu_count())])
     os.chdir("..")
 
 def lift_first_version_compiler():
     if not os.path.exists(BUILD_DIRECTORY):
         os.mkdir(BUILD_DIRECTORY)
+    lifted_ll = os.path.join(BUILD_DIRECTORY, "colgm_lifted.ll")
     execute([
         BOOTSTRAP_COMPILER,
         "--library", "src",
         "src/main.colgm",
-        "-o", "build/colgm_lifted.ll",
+        "-o",
+        lifted_ll,
         "--pass-info"
     ])
-    execute([
-        USED_CLANG,
-        "build/colgm_lifted.ll",
-        "-o", LIFTED_COMPILER,
-        "-g", "-Oz",
-        "-rdynamic", "-lm"
-    ])
+    if is_windows():
+        execute([
+            USED_CLANG,
+            lifted_ll,
+            "-o", LIFTED_COMPILER,
+            "-g", "-Oz"
+        ])
+    else:
+        execute([
+            USED_CLANG,
+            lifted_ll,
+            "-o", LIFTED_COMPILER,
+            "-g", "-Oz",
+            "-rdynamic", "-lm"
+        ])
 
 def build_self_host_compiler():
     if not os.path.exists(BUILD_DIRECTORY):
@@ -89,6 +127,9 @@ def build_self_host_compiler():
     ])
 
 def test_self_lift():
+    if is_windows():
+        print("Skipping self-lift test on Windows")
+        return
     execute([
         SELF_HOST_COMPILER,
         "--library", "src",
